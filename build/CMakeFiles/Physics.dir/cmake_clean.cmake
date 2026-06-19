b7237ea7c3b96fcfad4ecf55766c82703a82ac0f
Murad-D-11 <dashdamirov.murad11@gmail.com>
2026-06-19 11:49:38 -0400

    Integrate CMake

---
+++ build/CMakeFiles/Physics.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Physics.dir/src/glad.c.obj"
+  "CMakeFiles/Physics.dir/src/glad.c.obj.d"
+  "CMakeFiles/Physics.dir/src/main.cpp.obj"
+  "CMakeFiles/Physics.dir/src/main.cpp.obj.d"
+  "Physics.exe"
+  "Physics.exe.manifest"
+  "Physics.pdb"
+  "libPhysics.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Physics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
