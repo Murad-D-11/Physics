d6ea30eeaf99d2e59d1777cc3fc3793d1313f1ae
Murad-D-11 <dashdamirov.murad11@gmail.com>
2026-07-01 20:41:11 -0400

    Add comment to CollisionInfo

--- src/physics/CollisionInfo.h
+++ src/physics/CollisionInfo.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <glm/glm.hpp>
 
+/**
+ * All the info produced by one collision test between two AABBs
+ */
 struct CollisionInfo {
     bool collided = false; // true if the two AABBs overlap on all three axes
     glm::vec3 normal = glm::vec3(0.0f); // the axis along which the two bodies should be separated, pointing from body A toward body B, tells which direction to push each body
